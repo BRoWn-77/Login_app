67738e26d0c238400818b071eb7757f0e4ef6529
BRoWn-77 <mosesbinkat@gmail.com>
2024-02-12 22:43:46 +0100

    validate

--- client/login-app/src/helper/validate.js
+++ client/login-app/src/helper/validate.js
@@ -7,6 +7,30 @@ export async function usernameValidate(values){
     return errors;
 }
 
+/**validate password */
+export async function passwordValidate(values){
+    const errors = passwordVerify({}, values);
+
+    return errors;
+}
+
+/** validate password */
+function passwordVerify(errors = {}, values){
+    const specialChars = /[`!@#$%^&*()_+\-={};':"\\|,.<>?~]/;
+
+    if(!values.password){
+        errors.password = toast.error("Password Required...!");
+    } else if(values.password.includes(" ")){
+        errors.password = toast.error("Wrong Password...!");
+    } else if(values.password.length < 4){
+        errors.password = toast.error("Password must be more than 4 characters");
+    } else if(!specialChars.test(values.password)){
+        errors.password = toast.error("Password must have special characters");
+    }
+
+    return errors;
+}
+
 /** validate username */
 function usernameVerify(error = {}, values){
     if(!values.username){
